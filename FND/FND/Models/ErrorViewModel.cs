b88d4ce209dc46ace72e64bb39fab6173a4554a3
Mohamad nour <mn83680@gmail.com>
2024-12-23 21:38:14 +0300

    Background Home

--- FND/FND/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FND.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- FND/FND/Views/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NewsClassNet6.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
